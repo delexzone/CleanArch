f0c3147880070e398a1b894d88dc299be0bf526f
NISHLUK Alexey <delexzone@gmail.com>
2019-09-08 17:44:20 +0300

    Configure Presentation Layer for Dependency Container

--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CleanArch.Infra.Data.Context;
+using CleanArch.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
@@ -39,12 +40,17 @@ namespace CleanArch.Mvc
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("UniversityIdentityDBConnection")));
+            
+            services.AddDbContext<UniversityDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection")));
+            
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddDbContext<UniversityDbContext>(options => { options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection")); });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +80,11 @@ namespace CleanArch.Mvc
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
